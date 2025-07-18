00af29c38764817db0eb1b63484dc7e59922b628
Abdulghaffar39 <abdulghaffar15a@outlook.com>
2025-07-18 07:27:10 -0700

    navbar work

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 let menu_list = document.getElementById('menu_list');
+let nav = document.getElementById('nav');
 
 menu_list.style.maxHeight = '0px';
 
+
 function toggleMenu() {
 
-    if (menu_list.style.maxHeight == '0px') {
+    if (menu_list.style.maxHeight === '0px' ) {
 
         menu_list.style.maxHeight = '300px'
-        
+        menu_list.style.backgroundColor = 'rgb(31, 12, 6)';
+        nav.style.backgroundColor = 'rgb(31, 12, 6)';
 
     } else {
 
@@ -15,4 +18,11 @@ function toggleMenu() {
 
     }
 
-};
+}
+
+function hide_menu(){
+
+    menu_list.style.display = 'none'
+}
+
+
