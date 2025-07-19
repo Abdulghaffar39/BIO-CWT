75d54c40ec439e849b283815eb05d07d757310c1
Abdulghaffar39 <abdulghaffar15a@outlook.com>
2025-07-19 06:42:38 -0700

    navbar completed

--- app.js
+++ app.js
@@ -32,8 +32,8 @@ function toggleMenu_2(){
     if (menu_list.style.maxHeight === '300px' ) {
 
         menu_list.style.maxHeight = '0px';
-        menu_list.style.backgroundColor = 'rgb(31, 12, 6)';
-        nav.style.backgroundColor = 'rgb(31, 12, 6)';
+        menu_list.style.backgroundColor = 'transparent';
+        nav.style.backgroundColor = 'transparent';
         menu_icon_2.style.display = 'none';
         menu_bar.style.display = 'block';
 
