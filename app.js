b0f180640c968d736ded996cf0bae4d956b98b4b
Abdulghaffar39 <abdulghaffar15a@outlook.com>
2025-07-19 01:21:02 -0700

    navbar

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 let menu_list = document.getElementById('menu_list');
 let nav = document.getElementById('nav');
+let menu_bar = document.getElementById('menu_bar');
+let menu_icon_2 = document.getElementById('menu_icon_2');
 
 menu_list.style.maxHeight = '0px';
 
 
-function toggleMenu() {
 
+function toggleMenu() {
+    
+    
     if (menu_list.style.maxHeight === '0px' ) {
 
         menu_list.style.maxHeight = '300px'
         menu_list.style.backgroundColor = 'rgb(31, 12, 6)';
         nav.style.backgroundColor = 'rgb(31, 12, 6)';
+        menu_icon_2.style.display = 'block';
+        menu_bar.style.display = 'none';
 
     } else {
 
@@ -20,9 +26,23 @@ function toggleMenu() {
 
 }
 
-function hide_menu(){
+function toggleMenu_2(){
 
-    menu_list.style.display = 'none'
+
+    if (menu_list.style.maxHeight === '300px' ) {
+
+        menu_list.style.maxHeight = '0px';
+        menu_list.style.backgroundColor = 'rgb(31, 12, 6)';
+        nav.style.backgroundColor = 'rgb(31, 12, 6)';
+        menu_icon_2.style.display = 'none';
+        menu_bar.style.display = 'block';
+
+
+    } else {
+
+        menu_list.style.maxHeight == '0px'
+
+    }
 }
 
 
